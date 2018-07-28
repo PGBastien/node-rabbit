6891340721bd472b894b3b865a55b40b90039ac7
Bastien PELLETIER-GARNIER <bastien.pelletier.garnier@gmail.com>
2018-07-28 14:10:04 +0200

    Work queues : Add minor comments

--- work-queues/new_task.js
+++ work-queues/new_task.js
@@ -1,6 +1,8 @@
+// Require filesystem
 const fs = require('fs');
-
+// Require datetime
 var datetime = require('node-datetime');
+// Require lib amqp
 var amqp = require('amqplib');
 
 // ssl options
@@ -11,12 +13,14 @@ var opts = {
     ca: [fs.readFileSync('../certifs/ssl/ca/cacert.pem')]
 };
 
+// Connection to RabbitMQ with amqps protocol
 amqp.connect('amqps://moderation:moderation@skynet/moderation', opts).then(function(conn) {
     return conn.createChannel().then(function(ch) {
-        // Date time
+        // Get datetime now
         var dt = datetime.create();
         var formattedDate = dt.format('d/m/y H:M:S');
 
+        // Define queues
         var q = 'logs';
         var q2 = 'logs-bis';
         var ok = ch.assertQueue(q, {durable: true});
